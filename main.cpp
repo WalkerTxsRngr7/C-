e3fff20f39fa2b3777fc081bfcca8b782106e641
GROSS <wg0742700@otc.edu>
2019-08-27 14:38:47 -0500

    moved files

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int num;
+	
+	cout << "Hello, my name is Walker\n";
+	cout << "What is your favorite number?\n";
+	
+	cin >> num;
+	
+	cout << "So your favorite number is " << num;
+	cout << "\nThat is cool!\n";
+	cout << "My favorite number is 7";
+	
+	cout << endl;
+	cout << "On 7/7/07 my sister turned 7.";
+	
+	
+	
+	return 0;
+}
